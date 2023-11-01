79cca4bfcd5c72af35a03bc796237434f09b1386
bjayzee <bjayzee@gmail.com>
2023-11-01 15:01:55 +0100

    buy

--- app/repair/page.js
+++ app/repair/page.js
@@ -1,14 +1,3 @@
-
-import Repair from "@/components/Repair"
-
-const RepairPage = () => {
-  return (
-   <Repair/>
-  )
-}
-
-export default RepairPage
-
 import { Repair } from "@/components"
 
 const RepairPage = () => {
