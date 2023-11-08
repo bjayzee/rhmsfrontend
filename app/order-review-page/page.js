e16ee630f16768708c755f2367197323dee49dda
sondraycabana <sondraycabana@gmail.com>
2023-11-08 13:16:29 +0000

    Swap feature in developement

---
+++ app/order-review-page/page.js
@@ -0,0 +1,10 @@
+import OrderReviewPage from '@/components/OrderReviewPage'
+import React from 'react'
+
+const MyOrderReviewPage = () => {
+  return (
+    <OrderReviewPage/>
+  )
+}
+
+export default MyOrderReviewPage
