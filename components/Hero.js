95f6ca234bd60882d8e2c1add9b34102a5ca6e7e
sondraycabana <sondraycabana@gmail.com>
2023-11-28 21:40:43 +0000

    Updated hero page

--- components/Hero.js
+++ components/Hero.js
@@ -1,5 +1,4 @@
-"use client"
-
+"use client";
 import Image from 'next/image';
 import HeroCard from './HeroCard';
 import Link from 'next/link';
@@ -11,153 +10,130 @@ const Hero = () => {
     router.push(`/${name}`)
   }
   return (
-    <div>
-      <div>
-        <section className='flex justify-between gap-3 py-4 px-2'>
-          <div className=''>
-          <Link href='/swap-iphone-page'>
-           <div className='flex justify-evenly align-middle w-30 h-10 bg-white shadow-md rounded py-1'>
-          
-              <Image
-                src='/phoneicon.png'
-                height={20}
-                width={30} />
-                 
-              <Image
-                src='/swapicon.png'
-                height={10}
-                width={30} />
-              <Image
-                src='/iphoneicon.png'
-                height={20}
-                width={30} />
-               
-            </div>
-            </Link>
-            
-            <div>
-              <h2 className='text-rh-blue text-sm font-semibold'>Swap iPhones</h2>
-            </div>
+    <div className="px-5 overflow-x-hidden mt-7 bg-[#FFFFFF]">
+      <section className="flex justify-between gap-3 p-2">
+        <div
+          onClick={() => handleClick("swap-iphone-page")}
+          className="cursor-pointer flex flex-col items-center justify-center"
+        >
+          <div className="flex items-center justify-center w-24 h-16 bg-white shadow-2xl rounded-2xl p-3 border-[#D9D9D9] border-r-8 border-b-8">
+            <Image src="/phoneicon.png" height={20} width={30} />
+            <Image src="/swapicon.png" height={10} width={30} />
+            <Image src="/iphoneicon.png" height={20} width={30} />
           </div>
-          <div onClick={() => handleClick("swap-apple-watch")}>
-            <div className='flex justify-evenly align-middle w-30 h-10 bg-white shadow-md rounded py-1'>
-              <Image
-                src='/watch.png'
-                height={20}
-                width={30} />
-              <Image
-                src='/swapicon.png'
-                height={10}
-                width={20} />
-              <Image
-                src='/iwatchx.png'
-                height={20}
-                width={30} />
-            </div>
-            <div>
-              <h2 className='text-rh-blue text-sm font-semibold'>Swap iWatches</h2>
-            </div>
-          </div>
-          <div className='cursor-pointer' onClick={() => handleClick("buy")}>
-            <div className='flex justify-evenly align-center w-30 h-10 bg-white shadow-md rounded py-1'>
-              <Image
-                src='/iphoneicon.png'
-                height={30}
-                width={30} />
-            </div>
-            <div>
-              <h2 className='text-rh-blue text-sm font-semibold'>Buy iPhones</h2>
-            </div>
-          </div>
-        </section>
-        <section className='flex justify-between gap-3 py-4 px-2'>
-          <div className='' onClick={() => handleClick("buy-apple-watch")} >
-            <div className='flex justify-evenly align-middle w-30 h-10 bg-white shadow-md rounded py-1'>
-              <Image
-                src='/iwatchy.png'
-                height={20}
-                width={30} />
 
-            </div>
-            <div>
-              <h2 className='text-rh-blue text-sm font-semibold'>Buy iWatches</h2>
-            </div>
-          </div>
-          <div   onClick={() => handleClick("buy-mac")} >
-            <div className='flex justify-evenly align-middle w-30 h-10 bg-white shadow-md rounded py-1'>
+          <span className="text-rh-blue text-sm font-semibold text-center py-2">
+            Swap iPhones
+          </span>
+        </div>
 
-              <Image
-                src='/mac.png'
-                height={20}
-                width={90} />
-            </div>
-            <div>
-              <h2 className='text-rh-blue text-sm font-semibold'>Buy a MacBook</h2>
-            </div>
+        <div
+          className="cursor-pointer flex flex-col items-center justify-center"
+          onClick={() => handleClick("swap-apple-watch")}
+        >
+          <div className="flex items-center justify-center w-24 h-16 bg-white shadow-2xl p-3 rounded-2xl p-3 border-[#D9D9D9] border-r-8 border-b-8">
+            <Image src="/watch.png" height={100} width={100} />
+            <Image src="/swapicon.png" height={100} width={100} />
+            <Image src="/iwatchx.png" height={100} width={100} />
           </div>
-          <div  onClick={() => handleClick("buy-airpods")}>
-            <div className='flex justify-evenly align-center w-30 h-10 bg-white shadow-md rounded py-1'>
-              <Image
-                src='/pod.png'
-                height={30}
-                width={30} />
-            </div>
-            <div>
-              <h2 className='text-rh-blue text-sm font-semibold'>Buy Airpods</h2>
-            </div>
-          </div>
-        </section>
-        <section className='flex justify-between gap-3 py-4 px-2'>
-          <div className='' onClick={() => handleClick("buy-accessories")}>
-            <div className='flex justify-evenly align-middle w-30 h-10 bg-white shadow-md rounded py-1'>
-              <Image
-                src='/phoneicon.png'
-                height={20}
-                width={30} />
-              <Image
-                src='/swapicon.png'
-                height={10}
-                width={30} />
-              <Image
-                src='/iphoneicon.png'
-                height={20}
-                width={30} />
-            </div>
-            <div>
-              <h2 className='text-rh-blue text-sm font-semibold'>Buy Accessories</h2>
-            </div>
-          </div>
-          <div onClick={() => handleClick("buy-now-pay-later")}>
-            <div className='flex justify-evenly align-middle w-30 h-10 bg-[white] shadow-md rounded py-1'>
-              <Image
-                src='/basket.png'
-                height={20}
-                width={30} />
-            </div>
-            <div>
-              <h2 className='text-rh-blue text-sm font-semibold'>Buy and Pay Later</h2>
-            </div>
-          </div>
-          <div className='cursor-pointer' onClick={() => handleClick("repair")}>
-            <div className='flex justify-evenly align-center w-30 h-10 bg-white shadow-md rounded py-1'>
-              <Image
-                src='/repairicon.png'
-                height={30}
-                width={30} />
-            </div>
-            {/* </Link> */}
-            <div>
-              <h2 className='text-rh-blue text-sm font-semibold'>Repairs</h2>
-            </div>
-          </div>
-        </section>
+          <span className="text-rh-blue text-sm font-semibold text-center py-2">
+            Swap iWatches
+          </span>
+        </div>
 
-      </div>
-      {/* <div>
-            <h3>My Hero</h3><Link href="/Repair"><h3>Go to Repair</h3></Link>
-          </div> */}
+        <div
+          className="cursor-pointer flex flex-col items-center justify-center"
+          onClick={() => handleClick("buy")}
+        >
+          <div className="flex items-center justify-center w-24 h-16 bg-white shadow-2xl p-3 rounded-2xl p-3 border-[#D9D9D9] border-r-8 border-b-8">
+            <Image src="/iphoneicon.png" height={30} width={30} />
+          </div>
+
+          <span className="text-rh-blue text-sm font-semibold text-center py-2">
+            Buy iPhones
+          </span>
+        </div>
+      </section>
+
+      <section className="flex justify-between gap-3 p-2">
+        <div
+          className="cursor-pointer flex flex-col items-center justify-center"
+          onClick={() => handleClick("buy-apple-watch")}
+        >
+          <div className="flex items-center justify-center w-24 h-16 bg-white shadow-2xl rounded-2xl p-3 p-3 border-[#D9D9D9] border-r-8 border-b-8">
+            <Image src="/iwatchy.png" height={20} width={30} />
+          </div>
+          <span className="text-rh-blue text-sm font-semibold text-center py-2">
+            Buy iWatches
+          </span>
+        </div>
+
+        <div
+          className="cursor-pointer flex flex-col items-center justify-center"
+          onClick={() => handleClick("buy-mac")}
+        >
+          <div className="flex items-center justify-center w-24 h-16 bg-white shadow-2xl rounded-2xl p-3 border-[#D9D9D9] border-r-8 border-b-8">
+            <Image src="/mac.png" height={40} width={50} />
+          </div>
+          <span className="text-rh-blue text-sm font-semibold text-center py-2">
+            Buy MacBook
+          </span>
+        </div>
+
+        <div
+          className="cursor-pointer flex flex-col items-center justify-center"
+          onClick={() => handleClick("buy-airpods")}
+        >
+          <div className="flex items-center justify-center w-24 h-16 bg-white shadow-2xl rounded-2xl p-3 border-[#D9D9D9] border-r-8 border-b-8">
+            <Image src="/pod.png" height={20} width={30} />
+          </div>
+          <span className="text-rh-blue text-sm font-semibold text-center py-2">
+            Buy Airpods
+          </span>
+        </div>
+      </section>
+
+      <section className="flex justify-between gap-3 p-2">
+        <div
+          className="cursor-pointer flex flex-col items-center justify-center"
+          onClick={() => handleClick("buy-accessories")}
+        >
+          <div className="flex items-center justify-center w-24 h-16 bg-white shadow-2xl rounded-2xl p-3 border-[#D9D9D9] border-r-8 border-b-8">
+            <Image src="/phoneicon.png" height={20} width={30} />
+            <Image src="/swapicon.png" height={10} width={30} />
+            <Image src="/iphoneicon.png" height={20} width={30} />
+          </div>
+          <span className="text-rh-blue text-sm font-semibold text-center py-2">
+            Buy Accessories
+          </span>
+        </div>
+
+        <div
+          className="cursor-pointer flex flex-col items-center justify-center"
+          onClick={() => handleClick("buy-now-pay-later")}
+        >
+          <div className="flex items-center justify-center w-24 h-16 bg-white shadow-2xl rounded-2xl p-3 border-[#D9D9D9] border-r-8 border-b-8">
+            <Image src="/basket.png" height={20} width={30} />
+          </div>
+          <span className="text-rh-blue text-sm font-semibold text-center py-2">
+            Buy and Pay Later
+          </span>
+        </div>
+
+        <div
+          className="cursor-pointer flex flex-col items-center justify-center"
+          onClick={() => handleClick("repair")}
+        >
+          <div className="flex items-center justify-center w-24 h-16 bg-white shadow-2xl rounded-2xl p-3 border-[#D9D9D9] border-r-8 border-b-8">
+            <Image src="/repairicon.png" height={30} width={30} />
+          </div>
+          <span className="text-rh-blue text-sm font-semibold text-center py-2">
+            Repairs
+          </span>
+        </div>
+      </section>
     </div>
-  )
+  );
 }
 
 export default Hero
