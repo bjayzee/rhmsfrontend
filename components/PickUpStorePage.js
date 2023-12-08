db7733c01d1e646d7bb3e524ddc961ca2aa66f97
sondraycabana <sondraycabana@gmail.com>
2023-12-08 11:26:09 +0000

    Updated pickup stor form

--- components/PickUpStorePage.js
+++ components/PickUpStorePage.js
@@ -3,87 +3,47 @@ import Link from 'next/link';
 
 const PickUpStorePage = () => {
   return (
-    <div>
-       <div className='font-bold pl-5 m-5'>
+    <div className="px-5">
+       <div className='font-bold   text-xl my-5'>
             <h1>Pick up store:</h1>
         </div>
-        <div className='font-bold pl-5 m-3'>
+        <div className='font-bold  my-5'>
             <h2>We need your need your details to find you the nearest store</h2>
         </div>
-        <div className='pl-5'>
-            <input
-             style={{ 
-                border: '2px solid #187EB4',
-                padding: '8px 80px',
-                borderRadius:'10px',
-                color: '#187EB4', 
-                margin:'10px'
-                }}
+        
 
-                placeholder='First Name'
-            />
-
-            <input
-             style={{ 
-                border: '2px solid #187EB4',
-                padding: '8px 80px',
-                borderRadius:'10px',
-                color: '#187EB4', 
-                margin:'10px'
-                }}
-
-                placeholder='Last Name'
-            />
-
-<input
-             style={{ 
-                border: '2px solid #187EB4',
-                padding: '8px 80px',
-                borderRadius:'10px',
-                color: '#187EB4', 
-                margin:'10px'
-                }}
-
-                placeholder='Put your address'
-            />
-
-<input
-             style={{ 
-                border: '2px solid #187EB4',
-                padding: '8px 80px',
-                borderRadius:'10px',
-                color: '#187EB4', 
-                margin:'10px'
-                }}
-
-                placeholder='Email'
-            />
-
-<input
-             style={{ 
-                border: '2px solid #187EB4',
-                padding: '8px 80px',
-                borderRadius:'10px',
-                color: '#187EB4', 
-                margin:'10px'
-                }}
-
-                placeholder='Phone Number'
-            />
-        </div>
+        <div className="flex flex-col space-y-8 text-xl">
+        <input
+          className="focus:ring-0 outline-none border-2 border-[#187EB4] text-[#187EB4] rounded-2xl h-16 px-3"
+          placeholder="First Name"
+        />
+        <input
+          className="focus:ring-0 outline-none border-2 border-[#187EB4] text-[#187EB4] rounded-2xl h-16 px-3"
+          placeholder="Last Name"
+        />
+        <input
+          className="focus:ring-0 outline-none border-2 border-[#187EB4] text-[#187EB4] rounded-2xl h-16 px-3"
+          placeholder="Put your address"
+        />
+        <input
+          className="focus:ring-0 outline-none border-2 border-[#187EB4] text-[#187EB4] rounded-2xl h-16 px-3"
+          placeholder="Email"
+        />
+        <input
+          className="focus:ring-0 outline-none border-2 border-[#187EB4] text-[#187EB4] rounded-2xl h-16 px-3"
+          placeholder="Phone Number"
+        />  
+      </div>
 
         <div  className=" flex m-5   pl-10 font-bold">
-          <button  
-           style={{ 
-           border: '2px solid #187EB4',
-           padding: '5px 35px',
-           borderRadius:'20px',
-           color: 'white', 
-           backgroundColor: '#187EB4', 
-           margin:'10px'
-           }}>
-            <Link href="/guestOrderSuccessfulPage">CHECK STORE</Link> 
-          </button>
+         
+
+          <button
+            className="bg-[#187EB4] px-16 py-4 mt-5 rounded-full text-[#FFFFFF]"
+                              
+                            >
+              <Link href="/guestOrderSuccessfulPage">CHECK STORE</Link> 
+              </button>
           </div>
     </div>
   )
