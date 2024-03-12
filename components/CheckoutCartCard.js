ed20998bf0b9edc0c300c72247cb1e3df4b6ea81
Tejiri Anthony <tejirianthony07@gmail.com>
2024-03-12 12:40:33 +0100

    merge error fix

--- components/CheckoutCartCard.js
+++ components/CheckoutCartCard.js
@@ -61,47 +61,6 @@ const CheckoutCartCard = ({ phone }) => {
         </div>
       </div>
     );
-    temp[indexToBeReplaced] = tempItem;
-    setCartItems([...temp]);
-  };
-
-  return (
-    <div className="my-5">
-      <div className="flex flex-row items-center justify-between mb-2">
-        <img
-          src={phone.thumbnail}
-          alt={phone.name}
-          className="w-10 h-20 object-cover rounded-md mr-4"
-        />
-        <div className="flex flex-col mx-2">
-          <p className="text-sm font-bold">{phone.name}</p>
-          <p className="text-sm text-[gray]">
-            <span>Condition: </span>
-            {phone.specification.grade}
-          </p>
-          <p className="text-sm text-[gray]">
-            <span>Storage: </span>
-            {phone.specification.capacity}
-          </p>
-          <p className="text-sm text-[gray]">
-            <span>Color: </span>
-            {phone.specification.color}
-          </p>
-        </div>
-        <input
-          value={"qty: " + phone.quantity}
-          readOnly
-          className="w-10 mx-2"
-        />
-        <div className="flex items-center mb-2">
-          <span className="ml-2 font-semibold w-fit flex flex-row">
-            <TbCurrencyNaira className="h-6  " />
-            {(phone.price * phone.quantity)?.toLocaleString()}
-          </span>
-        </div>
-      </div>
-    </div>
-  );
 };
 
 export default CheckoutCartCard;
